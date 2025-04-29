050f9ac635302d6dcbc77873725eef8c83264cca
C111151158 <c111151158@nkust.edu.tw>
2025-04-29 21:47:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  worker: {
+    format: "es",
+    plugins: [react()], 
+  },
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // 後端
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), 
+      },
+    },
+  },
+
 });
