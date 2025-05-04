902875db59c3a05d6544db730ac3714b7228aeeb
C111151158 <c111151158@nkust.edu.tw>
2025-05-04 21:39:52 +0800

    Update fileWorker.js

--- src/HaplotypeNetwork/workers/fileWorker.js
+++ src/HaplotypeNetwork/workers/fileWorker.js
@@ -1,24 +1,30 @@
 self.onmessage = (event) => {
-  const fileContent = event.data;
+  const { type, fileContent } = event.data;
 
-  const geneNames = [];
-  const sequences = {};
-
-  let currentGene = null;
-  const lines = fileContent.split("\n");
-
-  lines.forEach((line) => {
-    if (line.startsWith(">")) {
-      currentGene = line.substring(1).trim();
-      geneNames.push(currentGene);
-      sequences[currentGene] = "";
-    } else if (currentGene) {
-      sequences[currentGene] += line.trim();
+  if (type === "parseFile") {
+    if (typeof fileContent !== "string") {
+      console.error("❌ fileContent 不是字串，實際為:", typeof fileContent);
+      return;
     }
-  });
 
-  
-  self.postMessage({ geneNames, sequences });
+    const geneNames = [];
+    const sequences = {};
+
+    let currentGene = null;
+    const lines = fileContent.split("\n");
+
+    lines.forEach((line) => {
+      if (line.startsWith(">")) {
+        currentGene = line.substring(1).trim();
+        geneNames.push(currentGene);
+        sequences[currentGene] = "";
+      } else if (currentGene) {
+        sequences[currentGene] += line.trim();
+      }
+    });
+
+    self.postMessage({ geneNames, sequences });
+  }
 };
 
 
