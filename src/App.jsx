402d6bb789a6fafa89bab6518aa60a9d6e5012c9
C111151158 <c111151158@nkust.edu.tw>
2025-05-05 19:17:37 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -135,7 +135,7 @@ function App() {
             />
             <Route
               path="/sequence-alignment"
-              element={<SequencealignmentAPP />} 
+              element={<SequencealignmentAPP haplotypeContent={haplotypeContent} />}
             />
           </Routes>
 
